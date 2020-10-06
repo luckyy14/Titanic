8f2bd463ee706d53fe918c080b897a0ac3893ddd
luckyy14 <42903815+luckyy14@users.noreply.github.com>
2020-10-06 19:06:36 +0530

    changed
    wd

---
+++ s.R
@@ -0,0 +1,8 @@
+setwd("C:\\Users\\laksh\\OneDrive\\Desktop\\Titanic")
+library(rsconnect)
+rsconnect::setAccountInfo(name='cyberalien',token='9BC16A69AEE8F48A5ED08E0C621C75A1',secret='94Nee1OyCtGDhhoCq6cTFwXMAeXUO5kydM+do9Rj')
+
+
+
+
+runGitHub( "Titanic", "luckyy14")
