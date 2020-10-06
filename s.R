4b8c082b760103b58fd77b253f1c7a676efbbd5d
luckyy14 <42903815+luckyy14@users.noreply.github.com>
2020-10-06 19:22:16 +0530

    s

--- s.R
+++ s.R
@@ -1,8 +1,4 @@
-setwd("C:\\Users\\laksh\\OneDrive\\Desktop\\Titanic")
 library(rsconnect)
+library(shiny)
 rsconnect::setAccountInfo(name='cyberalien',token='9BC16A69AEE8F48A5ED08E0C621C75A1',secret='94Nee1OyCtGDhhoCq6cTFwXMAeXUO5kydM+do9Rj')
-
-
-
-
 runGitHub( "Titanic", "luckyy14")
