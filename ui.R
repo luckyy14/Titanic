bd25c6ee53d88e54493259180eef6a0427ce08c1
luckyy14 <42903815+luckyy14@users.noreply.github.com>
2020-10-06 19:35:08 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -50,26 +50,6 @@ shinyUI(fluidPage(
             tags$li('cabin - Cabin'), 
             tags$li('cabin - Cabin')
             ),  
-		strong('SPECIAL NOTES:'),
-		p(' Pclass is a proxy for socio-economic status (SES)'),
-		p('1st ~ Upper; 2nd ~ Middle; 3rd ~ Lower'),
-		p('Age is in Years; Fractional if Age less than One (1)'),
-		p('If the Age is Estimated, it is in the form xx.5'),
-		p('With respect to the family relation variables (i.e. sibsp and parch)
-		some relations were ignored.  The following are the definitions used
-		for sibsp and parch.'),
-		p('Sibling:  Brother, Sister, Stepbrother, or Stepsister of Passenger Aboard Titanic'),
-		p('Spouse:   Husband or Wife of Passenger Aboard Titanic (Mistresses and Fiances Ignored)'),
-		p('Parent:   Mother or Father of Passenger Aboard Titanic'),
-		p('Child:    Son, Daughter, Stepson, or Stepdaughter of Passenger Aboard Titanic'),
-		p('Other family relatives excluded from this study include cousins,
-		nephews/nieces, aunts/uncles, and in-laws.  Some children travelled
-		only with a nanny, therefore parch=0 for them.  As well, some
-		travelled with very close friends or neighbors in a village, however,
-		the definitions do not support such relations.')
-			 
-			 ),
-	
 	 mainPanel(
 		h2('Basic Introductory Exploratory Analysis'),
 		plotOutput('plot'),
